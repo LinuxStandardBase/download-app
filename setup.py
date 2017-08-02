c07a6f9bec7a45808dc8424ec10cab5b75676ac7
Jeff Licquia <licquia@linuxfoundation.org>
2017-08-02 14:43:19 -0400

    Add Bootstrap support.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     long_description=__doc__,
     include_package_data=True,
     zip_safe=False,
-    install_requires=['Flask>=0.12', 'Flask-Script'],
+    install_requires=['Flask>=0.12', 'Flask-Script', 'flask-bootstrap'],
     packages=find_packages(),
     scripts=['bin/download_app.py']
 )
